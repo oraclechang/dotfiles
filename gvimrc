9e7bb548391fc9f74e39169bd13ceac5c9535881
Chuho Chang <oraclechang@gmail.com>
2015-01-25 00:19:16 +0900

    Add gvimrc(for macvim)

---
+++ gvimrc
@@ -0,0 +1,3 @@
+"set fencs=ucs-bom,utf-8,euc-kr.latin1
+"set fileencoding=euc-kr
+"set enc=euc-kr
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 #files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
-files="gitconfig gitignore tmux.conf vimrc gdbinit ackrc"    # list of files/folders to symlink in homedir
+files="gitconfig gitignore tmux.conf vimrc gvimrc gdbinit ackrc"    # list of files/folders to symlink in homedir
 
 ##########
 
