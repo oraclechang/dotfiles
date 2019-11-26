bd8c73bdf3f1e1a962fe6f5598f8096885be5f60
Chuho Chang <chuho.chang@sap.com>
2019-11-26 11:57:38 +0900

    Remove exa aliases

--- fish/functions/ll.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function ll --description 'alias ll=exa -l --get --classify'
-	exa -l --git --classify $argv;
-end
--- fish/functions/lls.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function lls --description 'alias ll=exa -l --get --classify -s mod'
-	exa -l --git --classify -s mod $argv;
-end
